d3c1a51c2b8e16c11054cb358e84ab8b9b6b3426
Jonas Leijzen <jonasleijzen@gmail.com>
2022-12-01 08:29:57 +0100

    Add project files.

---
+++ Day1_1/Program.cs
@@ -0,0 +1,23 @@
+﻿List<string> lines = File.ReadAllLines("data.txt").ToList();
+
+int max = 0;
+int sum = 0;
+
+for (int i = 0; i < lines.Count; i++) {
+    if (int.TryParse(lines[i], out int number)) {
+        sum += number;
+    } else {
+        if (sum > max) {
+            max = sum;
+        }
+        sum = 0;
+    }
+
+}
+
+if (sum > max) {
+    max = sum;
+}
+sum = 0;
+
+Console.WriteLine(max);
---
+++ Day1_2/Program.cs
@@ -0,0 +1,19 @@
+﻿List<string> lines = File.ReadAllLines("data.txt").ToList();
+
+List<int> sums = new List<int>();
+int sum = 0;
+
+for (int i = 0; i < lines.Count; i++) {
+    if (int.TryParse(lines[i], out int number)) {
+        sum += number;
+    } else {
+        sums.Add(sum);
+        sum = 0;
+    }
+
+}
+
+sums.Add(sum);
+
+int totalSum = sums.OrderByDescending(s => s).Take (3).Sum ();
+Console.WriteLine(totalSum);
