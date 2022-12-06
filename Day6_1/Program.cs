471fcf2de34f3e68a6204a013ddc3b63397ade16
Jonas Leijzen <jonasleijzen@gmail.com>
2022-12-06 10:46:19 +0100

    Day 6

---
+++ Day6_1/Program.cs
@@ -0,0 +1,12 @@
+﻿List<char> chars = File.ReadAllLines("data.txt")[0].ToCharArray().ToList ();
+
+for (int i = 3; i < chars.Count; i++) {
+    
+    bool areDifferent = chars.Skip(i - 3).Take(4).Distinct().Count() == 4;
+    if (areDifferent) {
+        Console.WriteLine(i + 1);
+        return;
+    }
+
+}
+
---
+++ Day6_2/Program.cs
@@ -0,0 +1,12 @@
+﻿List<char> chars = File.ReadAllLines("data.txt")[0].ToCharArray().ToList();
+
+for (int i = 3; i < chars.Count; i++) {
+
+    bool areDifferent = chars.Skip(i - 13).Take(14).Distinct().Count() == 14;
+    if (areDifferent) {
+        Console.WriteLine(i + 14);
+        return;
+    }
+
+}
+
