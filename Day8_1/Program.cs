216262a1e9a974de5df91061b5405030d1705466
Jonas Leijzen <jonasleijzen@gmail.com>
2022-12-08 23:44:00 +0100

    Day 8

---
+++ Day8_1/Program.cs
@@ -0,0 +1,70 @@
+﻿List<List<int>> trees = new List<List<int>>();
+
+string[] lines = File.ReadAllLines("data.txt");
+for (int i = 0; i < lines.Length; i++) {
+    trees.Add(new List<int>());
+    for (int j = 0; j < lines[i].Length; j++) {
+        trees[i].Add(int.Parse(lines[i][j].ToString ()));
+    }
+}
+
+List<List<bool>> visible = new List<List<bool>>();
+// init visible
+for (int i = 0; i < trees.Count; i++) {
+    visible.Add(new bool[trees[i].Count].ToList ());
+}
+
+// find all visible trees from every side
+// left
+for (int i = 0; i < trees.Count; i++) {
+    int max = -1;
+    for (int j = 0; j < trees[i].Count; j++) {
+        if (trees[i][j] > max) {
+            visible[i][j] = true;
+            max = trees[i][j];
+        }
+    }
+}
+
+// right
+for (int i = 0; i < trees.Count; i++) {
+    int max = -1;
+    for (int j = trees[i].Count - 1; j >= 0; j--) {
+        if (trees[i][j] > max) {
+            visible[i][j] = true;
+            max = trees[i][j];
+        }
+    }
+}
+
+// top
+for (int i = 0; i < trees[0].Count; i++) {
+    int max = -1;
+    for (int j = 0; j < trees.Count; j++) {
+        if (trees[j][i] > max) {
+            visible[j][i] = true;
+            max = trees[j][i];
+        }
+    }
+}
+
+// bottom
+for (int i = 0; i < trees[0].Count; i++) {
+    int max = -1;
+    for (int j = trees.Count - 1; j >= 0; j--) {
+        if (trees[j][i] > max) {
+            visible[j][i] = true;
+            max = trees[j][i];
+        }
+    }
+}
+
+// count the visibles
+int count = 0;
+for (int i = 0; i < visible.Count; i++) {
+    for (int j = 0; j < visible[i].Count; j++) {
+        count += visible[i][j] ? 1 : 0;
+    }
+}
+
+Console.WriteLine(count);
---
+++ Day8_2/Program.cs
@@ -0,0 +1,66 @@
+﻿List<List<int>> trees = new List<List<int>>();
+
+string[] lines = File.ReadAllLines("data.txt");
+for (int i = 0; i < lines.Length; i++) {
+    trees.Add(new List<int>());
+    for (int j = 0; j < lines[i].Length; j++) {
+        trees[i].Add(int.Parse(lines[i][j].ToString()));
+    }
+}
+
+long max = 0;
+for (int i = 0; i < trees.Count; i++) {
+    for (int j = 0; j < trees[i].Count; j++) {
+        long score = GetScenicScore(i, j);
+        if (score > max) {
+            max = score;
+        }
+    }
+}
+Console.WriteLine(max);
+
+long GetScenicScore (int x, int y) {
+
+    int right = 0;
+    for (int i = x + 1; i < trees[y].Count; i++) {
+        right++;
+        if (trees[y][i] >= trees[y][x]) {
+            break;
+        }
+    }
+
+    int left = 0;
+    for (int i = x - 1; i >= 0; i--) {
+        left++;
+        if (trees[y][i] >= trees[y][x]) {
+            break;
+        }
+    }
+
+    int bottom = 0;
+    for (int i = y + 1; i < trees.Count; i++) {
+        bottom++;
+        if (trees[i][x] >= trees[y][x]) {
+            break;
+        }
+    }
+    
+    int top = 0;
+    for (int i = y - 1; i >= 0; i--) {
+        top++;
+        if (trees[i][x] >= trees[y][x]) {
+            break;
+        }
+  
+    }
+
+    /*
+    Console.WriteLine("left: " + left);
+    Console.WriteLine("right: " + right);
+    Console.WriteLine("top: " + top);
+    Console.WriteLine("bottom: " + bottom);
+    */
+    
+    return left * right * bottom * top;
+
+}
