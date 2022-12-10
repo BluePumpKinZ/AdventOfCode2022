b5792a24ae23ad39ff50737b0de6a3cb29b4b1c9
Jonas Leijzen <jonasleijzen@gmail.com>
2022-12-10 16:52:23 +0100

    Day 10

---
+++ Day10_1/Program.cs
@@ -0,0 +1,38 @@
+﻿List<string> lines = File.ReadAllLines("data.txt").ToList();
+
+int x = 1;
+int cycle = 0;
+
+List<(int cycle, int s)> cycleStrengths = new ();
+
+for (int i = 0; i < lines.Count; i++) {
+    ExecuteLine(lines[i]);
+}
+
+void TickUpCycle() {
+    cycle++;
+
+    CheckCycle();
+}
+
+void CheckCycle () {
+    if ((cycle + 20) % 40 == 0) {
+        cycleStrengths.Add((cycle, cycle * x));
+        Console.WriteLine($"Cycle: {cycle}, X: {x}, SignalStrength: {cycle * x}");
+    }
+}
+
+void ExecuteLine (string line) {
+    if (line.Equals("noop"))
+    {
+        TickUpCycle();
+        return;
+    }
+    TickUpCycle();
+    TickUpCycle();
+    int value = int.Parse(line.Substring(5));
+    x += value;
+    
+}
+
+Console.WriteLine(cycleStrengths.Where(v => v.cycle is >= 20 and <= 220).Sum(v => v.s));
---
+++ Day10_2/Program.cs
@@ -0,0 +1,39 @@
+﻿List<string> lines = File.ReadAllLines("data.txt").ToList();
+
+int x = 1;
+int cycle = 1;
+
+List<(int cycle, int s)> cycleStrengths = new();
+
+for (int i = 0; i < lines.Count; i++) {
+    ExecuteLine(lines[i]);
+}
+
+void TickUpCycle () {
+
+    bool contained = Math.Abs(cycle % 40 - x) <= 1;
+    Console.Write(contained ? "#" : ".");
+
+    CheckLine();
+
+    cycle++;
+}
+
+void CheckLine () {
+    if (cycle % 40 == 0) {
+        Console.WriteLine();
+    }
+}
+
+void ExecuteLine (string line) {
+    if (line.Equals("noop")) {
+        TickUpCycle();
+        return;
+    }
+    TickUpCycle();
+    
+    int value = int.Parse(line.Substring(5));
+    x += value;
+    TickUpCycle();
+    
+}
